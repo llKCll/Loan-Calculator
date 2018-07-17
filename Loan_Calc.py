456e9a23f3402a3c58e4e2fd6f4c1705910ad4fa
Kevin Conner <connerke@oregonstate.edu>
2018-07-16 19:40:02 -0500

    Validate user entries

--- Loan_Calc.py
+++ Loan_Calc.py
@@ -27,40 +27,79 @@ interest_entry.grid(row=1, column=1, padx = 20)
 period_entry = tk.Entry(payment_app)
 period_entry.grid(row=2, column=1, padx = 20)
 
-payment_entry = tk.Entry(payment_app, state='readonly')
+payment_entry = tk.Entry(payment_app, state="readonly")
 payment_entry.grid(row=3, column=1, padx = 20)
+
+'''
+    Returns true if the string a integer or a float, false otherwise.
+    Args - A string to validate. Entry is the type of string such as amount, rate, or years
+    for more accurate invalid print statements. 
+'''
+def validate(num_string, entry):
+    periods = 0
+
+    # Check for multiple decimals and spaces.
+    for char in num_string:
+        if char == ' ':
+            print("Invalid {} input. Please enter an integer or decimal without spaces that is greater than 0.".format(entry))
+            return False
+        
+        if char == '.':
+            periods += 1
+
+    if periods > 1:
+        print("Invalid {} input. Please enter an integer or decimal without spaces that is greater than 0.".format(entry))
+        return False
+
+    # Ensure no letters or symbols were used.
+    for char in num_string:
+        if char.isalpha():
+            print("Invalid {} input. Please enter an integer or decimal without spaces that is greater than 0.".format(entry))
+            return False
+
+        if not char.isdigit() and char != '.':
+            print("Invalid {} input. Please enter an integer or decimal without spaces that is greater than 0.".format(entry))
+            return False
+    
+    # Ensure the number is greater than 0.
+    if float(num_string) <= 0:
+        print("Invalid {} input. Please enter an integer or decimal without spaces that is greater than 0.".format(entry))
+        return False
+
+    return True
 
 '''
     Calculates loan amount when given loan amount, interest rate, and loan period in years.
     Push compute button and answer appears in monthly payment box
     checks if loan amount > 0 and for a string with letters
 '''
-def calc(): 
-    amount = amount_entry.get()
-    rate = interest_entry.get()
-    years = period_entry.get()
+def calc():
+    # Get and emove possible leading and trailing spaces from entries.
+    amount = amount_entry.get().strip()
+    rate = interest_entry.get().strip()
+    years = period_entry.get().strip()
+
+
+    if validate(amount, "amount") and validate(rate, "rate") and validate(years, "loan period"):
     
-    # Check for letters
-    if not amount.isalpha() and not rate.isalpha() and not years.isalpha(): 
         amount = float(amount)
         rate = float(rate)
         years = float(years)
-
+                
         # Calculate the result.
-        if amount > 0:
-            r = (rate / 100) / 12
-            p = 12 * years
-            payment = (r * amount) / (1-(1+r)**-p)
-            payment = round(payment, 2)
-            payment = str(payment)
-            
-            # Update the montly payment result in the entry box.
-            payment_entry.configure(state='normal')
-            payment_entry.delete(0, tk.END)
-            payment_entry.insert(0, '$'+payment)
-            payment_entry.configure(state='readonly')
-    #end if
-#end def
+        r = (rate / 100) / 12
+        p = 12 * years
+        payment = (r * amount) / (1-(1+r)**-p)
+        #payment = round(payment, 2)
+        payment = "%0.2f" % payment
+        payment = str(payment)
+        
+        # Update the montly payment result in the entry box.
+        payment_entry.configure(state='normal')
+        payment_entry.delete(0, tk.END)
+        payment_entry.insert(0, '$'+payment)
+        payment_entry.configure(state='readonly')
+
 
 # Create the button to calculate the entries.
 compute_button = tk.Button(payment_app, text='Compute Payment', command=calc) 
